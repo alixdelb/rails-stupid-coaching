731cb108060eb857b671da9898b74ad437db6daf
Alix Delbarre <alix-d@hotmail.fr>
2018-02-12 14:00:42 +0100

    stupid coaching on rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+
+  def answer
+    @question = params[:question]
+    return @answer = coach_answer(@question)
+  end
+
+
+  def coach_answer(question)
+    if question == "I am going to work"
+      return "Great!"
+    elsif question.end_with?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
